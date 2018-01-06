02344fc90eb69f9a7f8cf76c67e3e6b9a499f66c
Zekang Wu <zekang.wu@gmail.com>
2018-01-06 14:02:47 +0800

    add bottle jump

--- src/index.js
+++ src/index.js
@@ -34,16 +34,13 @@ camera.castShadow = true;
 camera.receiveShadow = true;
 camera.lookAt(new THREE.Vector3(0,0,0))
 
-const cube = new THREE.Mesh(new THREE.CubeGeometry(1,1,1), new THREE.MeshLambertMaterial({color: 0x00ffff}));
-cube.position.y = 0.5;
-cube.castShadow = true;
-cube.receiveShadow = true;
-const group = new THREE.Group();
-group.add(cube);
 
-scene.add(group);
+let block = nextBlock();
+let next = nextBlock(block);
+scene.add(block);
+scene.add(next);
 
-const ground = new THREE.Mesh(new THREE.PlaneGeometry(3, 3, 200, 200), new THREE.ShadowMaterial({transparent: true, opacity: 0.3, color: 0x000000}));
+const ground = new THREE.Mesh(new THREE.PlaneGeometry(200, 200, 200, 200), new THREE.ShadowMaterial({transparent: true, opacity: 0.3, color: 0x000000}));
 ground.rotation.set(Math.PI / -2, 0, 0);
 ground.receiveShadow = true;
 ground.castShadow = true;
@@ -56,23 +53,57 @@ scene.add(background);
 
 var loader = new THREE.ObjectLoader();
 
-loader.parse(require('./models/bottle.json'), object => {
-  const player = new THREE.Group();
-  player.position.y = 1;
-  player.add(object);
-  scene.add(player);
+var object = loader.parse(require('./models/bottle.json'));
 
-  new TWEEN.Tween({cubeScaleY: 1, playerScaleY: 1, playerScaleXZ: 1})
-      .to({cubeScaleY : 0.5, playerScaleY: 0.65, playerScaleXZ: 1.5}, 2000)
-      .easing(TWEEN.Easing.Quadratic.Out)
-      .onUpdate(({cubeScaleY, playerScaleY, playerScaleXZ}) => {
-    group.scale.setY(cubeScaleY);
-    player.position.y = cubeScaleY;
-    player.scale.set(playerScaleXZ, playerScaleY, playerScaleXZ);
-  }).start();
 
-});
+const player = new THREE.Group();
+player.position.y = 1;
+player.add(object);
+scene.add(player);
 
+var props = {cubeScaleY: 1, playerScaleY: 1, playerScaleXZ: 1};
+
+const _update = ({cubeScaleY, playerScaleY, playerScaleXZ}) => {
+  block.scale.setY(cubeScaleY);
+  player.position.y = cubeScaleY;
+  player.scale.set(playerScaleXZ, playerScaleY, playerScaleXZ);
+}
+
+const press = new TWEEN.Tween(props)
+    .to({cubeScaleY : 0.5, playerScaleY: 0.65, playerScaleXZ: 1.5}, 2000)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onUpdate(_update);
+
+const bounce = () => {
+  return new TWEEN.Tween(props).to({cubeScaleY: 1, playerScaleY: 1, playerScaleXZ: 1}, 500)
+    .easing(TWEEN.Easing.Bounce.Out).onUpdate(_update);
+}
+
+
+function nextBlock(prev = null) {
+  let block = createBlock();
+  if (prev) {
+    const {x, z} = prev.position;
+    block.position.set(x + Math.random() * 1.4 + 1.2, 0, z);
+  }
+  blockDown(block);
+  return block;
+}
+
+function blockDown(block) {
+  block.position.y = 0.5;
+  return new TWEEN.Tween(block.position).to({y: 0}, 500).easing(TWEEN.Easing.Quadratic.Out).start();
+}
+
+function createBlock() {
+  const cube = new THREE.Mesh(new THREE.CubeGeometry(1,1,1), new THREE.MeshLambertMaterial({color: 0x00ffff}));
+  cube.position.y = 0.5;
+  cube.castShadow = true;
+  cube.receiveShadow = true;
+  const block = new THREE.Group();
+  block.add(cube);
+  return block;
+}
 
 function animate(time) {
   requestAnimationFrame(animate);
@@ -88,3 +119,26 @@ render();
 requestAnimationFrame(animate);
 
 
+function onTouchStart(e) {
+  press.start();
+}
+
+function onTouchEnd(e) {
+  press.stop();
+  console.log(e);
+  bounce().start();
+
+  const distance = 2,
+        height = 2,
+        duration = 500;
+  
+  new TWEEN.Tween(player.position).to({x: distance}, duration).start();
+  const up = new TWEEN.Tween(player.position).to({y: 1 + height}, duration / 2).easing(TWEEN.Easing.Quadratic.Out);
+  const down = new TWEEN.Tween(player.position).to({y: 1}, duration / 2).easing(TWEEN.Easing.Quadratic.In);
+  new TWEEN.Tween(player.rotation).to({z: -2 * Math.PI}, duration).start();
+  up.chain(down).start();
+}
+
+document.addEventListener("touchstart", onTouchStart);
+
+document.addEventListener("touchend", onTouchEnd);
